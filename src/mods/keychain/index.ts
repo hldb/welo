ab233a01cba0ee2c69444ba388271bd18d6cb724
tabcat <tabcat00@proton.me>
2022-09-07 17:09:50 -0500

    chore: make Keychain options optional

--- src/mods/keychain/index.ts
+++ src/mods/keychain/index.ts
@@ -17,7 +17,7 @@ export const defaultOptions = {
 }
 
 class Keychain extends KeyChain {
-  constructor (datastore: StorageReturn, options: KeyChainInit) {
+  constructor (datastore: StorageReturn, options?: KeyChainInit) {
     const components: any = { getDatastore: () => datastore }
     super(components as Components, defaultOptions)
   }
