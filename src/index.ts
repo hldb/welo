9a0656a657c233f1c29dbcf848bf3ac50bfcf607
tabcat <tabcat00@proton.me>
2022-09-28 14:05:26 -0500

    update import paths; no replicator yet

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import { Opal as _Opal } from './opal.js'
 
 import { LevelStorage } from './mods/storage.js'
 import { Keychain } from './mods/keychain/index.js'
-import { Replicator } from './database/replicator/index.js'
+// import { Replicator } from './mods/replicator/index.js'
 
-import { StaticAccess } from './formats/access/default/index.js'
-import { Entry } from './formats/entry/default/index.js'
-import { Identity } from './formats/identity/default/index.js'
-import { Keyvalue } from './formats/store/keyvalue/index.js'
+import { StaticAccess } from './access/default/index.js'
+import { Entry } from './entry/default/index.js'
+import { Identity } from './identity/default/index.js'
+import { Keyvalue } from './store/keyvalue/index.js'
 
 _Opal.registry.access.add(StaticAccess)
 _Opal.registry.entry.add(Entry)
@@ -16,6 +16,6 @@ _Opal.registry.store.add(Keyvalue)
 
 _Opal.Storage = LevelStorage
 _Opal.Keychain = Keychain
-_Opal.Replicator = Replicator
+// _Opal.Replicator = Replicator
 
 export const Opal = _Opal
