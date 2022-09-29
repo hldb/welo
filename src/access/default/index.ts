330519baf2837a87ca2d19e31d4052bc248c783c
tabcat <tabcat00@proton.me>
2022-09-28 19:32:02 -0500

    cleanup access

--- src/access/default/index.ts
+++ src/access/default/index.ts
@@ -1,21 +1,21 @@
 // static access controller
 
 import { base32 } from 'multiformats/bases/base32'
-import { Manifest } from '../../manifest/default/index.js'
 import { wildcard } from '../util.js'
 import { AccessInstance, AccessStatic } from '../interface.js'
 import { Extends } from '../../decorators'
-import protocol, { Access, Config } from './protocol.js'
+import protocol, { AccessProtocol, Config } from './protocol.js'
 import { EntryInstance } from '../../entry/interface.js'
 import { ManifestInstance, ManifestData } from '../../manifest/interface.js'
 
 interface ManifestValue extends ManifestData {
-  access: Access
+  access: AccessProtocol
 }
 
 @Extends<AccessStatic>()
-class StaticAccess implements AccessInstance {
-  readonly manifest: Manifest
+// the Static in StaticAccess means the ACL is immutable and does not change
+export class StaticAccess implements AccessInstance {
+  readonly manifest: ManifestInstance<ManifestValue>
   readonly config: Config
   readonly write: Set<string>
 
@@ -28,7 +28,7 @@ class StaticAccess implements AccessInstance {
   start (): void {
     if (this.isStarted()) { return }
 
-    if (!Array.isArray(this.config?.write) || this.config?.write.length === 0) {
+    if (!Array.isArray(this.config.write) || this.config.write.length === 0) {
       throw new Error(
         'manifest.access.write does not grant access to any writers'
       )
@@ -44,8 +44,12 @@ class StaticAccess implements AccessInstance {
 
   constructor ({ manifest }: { manifest: ManifestInstance<ManifestValue> }) {
     this.manifest = manifest
-    this.config = manifest.access.config
+    this.config = manifest?.access?.config
     this._isStarted = false
+
+    if (!Array.isArray(this.config?.write)) {
+      throw new Error('expected manifest.access.config.write to be an array')
+    }
 
     this.write = new Set(
       this.config.write.map((w: Uint8Array | string) =>
@@ -68,5 +72,3 @@ class StaticAccess implements AccessInstance {
     return this.write.has(string) || this.write.has(wildcard)
   }
 }
-
-export { StaticAccess }
