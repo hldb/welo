41d860b6d740bb331a40dc82236e086c635dfb60
tabcat <tabcat00@proton.me>
2022-12-07 13:19:42 -0600

    use Replicator/ReplicatorClass types

--- src/interface.ts
+++ src/interface.ts
@@ -8,16 +8,15 @@ import type { IdentityProtocol } from '~identity/basal/protocol.js'
 import type { IdentityInstance, IdentityStatic } from '~identity/interface.js'
 import type { Blocks } from '~blocks/index.js'
 import type { StoreProtocol } from '~store/keyvalue/protocol.js'
-import type { Replicator } from '~replicator/index.js'
 import type { KeyChain } from '~utils/types.js'
 import type { DatastoreClass } from '~utils/datastore.js'
 import type { Address } from '~manifest/index.js'
-import type { MultiReplicator } from '~replicator/multi.js'
 import type { ManifestInstance } from '~manifest/interface.js'
 import type { AccessInstance, AccessStatic } from '~access/interface.js'
 import type { EntryStatic } from '~entry/interface.js'
 import type { StoreInstance, StoreStatic } from '~store/interface'
 import type { Replica } from '~replica/index.js'
+import type { Replicator, ReplicatorClass } from '~replicator/interface'
 
 export interface Create {
   directory?: string
@@ -51,7 +50,7 @@ export interface Determine {
 export interface Options {
   identity?: IdentityInstance<any>
   Datastore?: DatastoreClass
-  Replicator?: typeof Replicator
+  Replicator?: ReplicatorClass
 }
 
 interface AddressEmit {
@@ -71,7 +70,7 @@ export interface DbOpen {
   Datastore: DatastoreClass
   start?: boolean
   blocks: Blocks
-  Replicator: typeof MultiReplicator
+  Replicator: ReplicatorClass
   ipfs?: IPFS
   libp2p?: Libp2p
   identity: IdentityInstance<any>
@@ -83,7 +82,7 @@ export interface DbOpen {
 }
 
 export interface DbConfig extends Omit<DbOpen, 'start'> {
-  replicator: MultiReplicator
+  replicator: Replicator
   replica: Replica
   store: StoreInstance
   access: AccessInstance
