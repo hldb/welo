75758d41be8c544b8363212838ad42e36e10a442
tabcat <tabcat00@proton.me>
2022-12-17 13:28:07 -0600

    remove unused WELO_LOWER

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,3 @@
-export const WELO_LOWER: 'welo' = 'welo'
+export const WELO_PATH: '/welo' = '/welo'
 
-export const WELO_PATH: '/welo' = `/${WELO_LOWER}`
-
-export const OPALSNT_PREFIX : '/opalsnt/' = '/opalsnt/'
+export const OPALSNT_PREFIX: '/opalsnt/' = '/opalsnt/'
