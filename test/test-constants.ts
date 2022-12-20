647be43c0709df4971b6cf413aff1e29fa84b97f
tabcat <tabcat00@proton.me>
2022-12-20 17:51:28 -0600

    fix: update constants test for rename

--- test/test-constants.ts
+++ test/test-constants.ts
@@ -3,11 +3,11 @@ import { strict as assert } from 'assert'
 import * as constants from '~utils/constants.js'
 
 describe('Constants', () => {
-  it('exports WELO_LOWER', () => {
-    assert.equal(constants.WELO_LOWER, 'welo')
+  it('exports WELO_PATH', () => {
+    assert.equal(constants.WELO_PATH, '/welo')
   })
 
   it('exports OPALSNT_PREFIX', () => {
-    assert.equal(constants.WELO_PATH, '/welo')
+    assert.equal(constants.OPALSNT_PREFIX, '/opalsnt/')
   })
 })
