ff77afd6056afff23c25604a156180663ab6335e
saul <saul@organicdesign.co.nz>
2023-05-24 17:11:47 +1200

    Move default welo creation to src util.

--- src/utils/createDefaultWelo.ts
+++ src/utils/createDefaultWelo.ts
@@ -5,7 +5,7 @@ import { Identity } from '@/identity/basal/index.js'
 import { Keyvalue } from '@/store/keyvalue/index.js'
 import type { Create } from '@/interface.js';
 
-export const createWelo = (config: Omit<Create, "handlers">) => Welo.create({
+export default (config: Omit<Create, "handlers">) => Welo.create({
   handlers: {
     identity: [Identity],
     access: [StaticAccess],
