2dd9f6d4217dc5cc1f533ec86032c5eb0bd313e6
tabcat <tabcat00@proton.me>
2023-05-28 17:53:47 -0500

    add _all and _allKeys to Paily

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,11 +1,5 @@
 import type { Libp2p } from 'libp2p'
-import type { BaseDatastore } from 'datastore-core'
-import type { AnyLink } from '@alanshaw/pail/link'
 
 export type KeyChain = Libp2p['keychain']
-
-export interface IpldDatastore<L extends AnyLink> extends BaseDatastore {
-  root: L
-}
 
 // export interface FetchOptions extends AbortOptions, PreloadOptions {}
