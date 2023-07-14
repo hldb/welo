e3502349c192f7dede178979b4d39f4d35238706
saul <saul@organicdesign.co.nz>
2023-07-14 17:09:23 +1200

    wip: add create filter and get difference methods

--- src/utils/replicator.ts
+++ src/utils/replicator.ts
@@ -3,13 +3,36 @@ import { parsedcid } from '@/utils/index.js'
 import { dagLinks, loadEntry, traverser } from '@/replica/traversal.js'
 import { Heads } from '@/message/heads.js'
 import { CID } from 'multiformats/cid'
+import { BloomFilter } from 'fission-bloom-filters'
 import type { DbComponents } from '@/interface.js'
 import type { Replica } from '@/replica/index.js'
+
+const filterSize = (length: number, rate: number): number =>
+  Math.ceil(-((length * Math.log(rate)) / Math.pow(Math.log(2), 2)))
+
+const filterHashes = (size: number, length: number): number =>
+  Math.ceil((size / length) * Math.log(2))
 
 export const getHeads = async (replica: Replica): Promise<CID[]> => {
   const keys = await all(replica.heads.queryKeys({}))
 
   return keys.map(key => parsedcid(key.baseNamespace()))
+}
+
+export const createFilter = async (replica: Replica, options: Partial<{ errorRate: number }> = {}): Promise<{ filter: Uint8Array, hashes: number }> => {
+  const heads = await getHeads(replica)
+  const size = filterSize(heads.length, options.errorRate ?? 0.1)
+  const hashes = filterHashes(size, heads.length)
+  const filter = new BloomFilter(size, hashes)
+
+  return { filter: filter.toBytes(), hashes }
+}
+
+export const getDifference = async (replica: Replica, filter: Uint8Array, hashes: number): Promise<CID[]> => {
+  const filterObj = BloomFilter.fromBytes(filter, hashes)
+  const heads = await getHeads(replica)
+
+  return heads.filter(h => !filterObj.has(h.bytes))
 }
 
 export const encodeHeads = (cids: CID[]): Uint8Array => {
