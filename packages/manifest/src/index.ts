7deaddca34d863b5bca88a771aa20805b41d89b8
tabcat <tabcat00@proton.me>
2024-01-17 17:24:01 -0600

    change manifest generic type

--- packages/manifest/src/index.ts
+++ packages/manifest/src/index.ts
@@ -1,7 +1,7 @@
 import type { Access } from '@welo/access'
 import type { Protocol } from './protocol.js'
 
-export interface Manifest <T extends Record<string, any> = Record<string, never>>{
+export interface Manifest <T extends any = unknown> {
   protocol: Protocol
   params: {
     topic: string
